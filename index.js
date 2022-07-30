805b2d113cba3e549873537d7aefb0c1443253ea
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2022-07-29 22:15:16 -0400

    Add launchRadLab Cloud Function

---
+++ index.js
@@ -0,0 +1,26 @@
+const functions = require('@google-cloud/functions-framework');
+const {PubSub} = require('@google-cloud/pubsub');
+
+functions.http('launchRadLab', async (req, res) => {
+  const pubsub = new PubSub()
+
+  const {module} = req.body
+
+  switch (module) {
+    case 'data-science':
+      await launchDataScience(pubsub)
+      return res.status(201).json({})
+      default:
+        return res.status(400).json(`Not implemented for module ${module}`)
+  }
+})
+
+/**
+ * Launches a RAD Lab data_science module
+ * @param {PubSub} pubsub 
+ */
+async function launchDataScience(pubsub) {
+  const topic = pubsub.topic('rad-lab-launch-data-science')
+  const data = Buffer.from(JSON.stringify({}))
+  await topic.publishMessage({data})
+}
